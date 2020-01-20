81ee62be7e6f9f313896de0f33aaf5ecde8eddd0
Nayara Tostes <nayara_tostes_dm@hotmail.com>
2020-01-20 18:07:13 -0200

    Aula 01

---
+++ Aulas/Aula01.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*- 
+# Números e Operadores
+'''
+comentar 
+linhas
+multiplas
+(isso nao é apenas um comentario)
+'''
+
+#Aritimeticos
+
+# print(10 + 15)
+# print(30 - 15)
+# print(10 * 5)
+# print(10/2)
+# print(10//3)
+# print(10%3)
+# print(10**(9/2))
+
+
+#Atribuição 
+# x = 10
+# x = 2
+
+# x += y
+# x = x + y 
+# print(x)
+
+#Comparação
+
+# y = 15
+# y =25
+
+# print(x == y) igualdade
+# print(x >= y) maior igual 
+
+
+# Entrada e saída de dados 
+
+#METODOS DE CONVERSAO
+ 
+#  int()
+#  float()
+#  str()
+#  bool()
+
+
+# entrada = input('Digite a entrada: ')
+# print(entrada)
+
+#conversão de tipos (após convertido ele soma)
+# x = int(input('Digite o valor de x'))
+# y = int(input('Digite o valor de y'))
+
+# print(x + y)
+
+
+# Estruturas de Dados 
+
+#Lista
+# [] sempre em cochetes é uma lista, uma junção de dados 
+#vc pode colocar tudo junto conforme abaixo
+
+lista = [1, 1.6, 'string', True[1,2,2,4,4]]
+
+#Fatiamento de Lista
+
+print(lista[4][2])
+
+#Metodos de lista
+#-append = adiciona no final
+
+
+
+# lista.append(100)
+# lista.insert()
+# print(lista)
+
+#Tupla
+#diferença de uma tupla pra um lista? não é chochetes e sim parentes e ela é imutavel
+#durante a execuçção ngm pode mudar 
+
+
+itens = (1,2)
+itens.count (2)
+
+
+
---
+++ Exercicios/Ex_Aula01.py
@@ -0,0 +1,34 @@
+#Faça um programa de calculo de média 
+#que receba 4 notas e retorne a média 
+
+# Nota1 = float(input('Digite a Nota 1'  ))
+# Nota2 = float(input('Digite a Nota 2'  ))
+# Nota3 = float(input('Digite a Nota 3'  ))
+# Nota4 = float(input('Digite a Nota 4'  ))
+
+# #estaciar em uma variavel
+
+# media = (Nota1 + Nota2 + Nota3 + Nota4) / 4
+
+# print(media)
+
+
+#Listas
+
+#produtos = ['Camiseta',['p','m','g'], 'Calça', ['40', '42', '48'], ['branca','amarela','verde']]
+
+# Faça um programa que imprima na tela as seguintes informações
+
+# 1 - Camiseta, branca, G
+
+# 2 - Calça, 42 verde
+
+# 3 - Camiseta, branca, 42
+
+#print(produtos) = ['0',['3'], 'Calça', ['40', '42', '48'], ['branca','amarela','verde']]
+
+#maneira certa
+
+print(produtos[0], produtos[4][0],produtos [1][2])
+print(produtos[2], produtos[3][1],produtos [4][2])
+print(produtos[0], produtos[4][0],produtos [3][1])
